f007c26381945cb28f1bcf98a9443c8a31afefa4
gabijaatmanaviciute <48480494+gabijaatmanaviciute@users.noreply.github.com>
2021-07-25 00:08:00 +0300

    initial setup + routing started

---
+++ app/Http/Controllers/MechanicController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mechanic;
+use Illuminate\Http\Request;
+
+class MechanicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $mechanics = Mechanic::all();
+        return view('mechanic.index', ['mechanics' => $mechanics]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('mechanic.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $mechanic = new Mechanic;
+        $mechanic->name = $request->mechanic_name;
+        $mechanic->surname = $request->mechanic_surname;
+        $mechanic->save();
+        return redirect()->route('mechanic.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Mechanic  $mechanic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mechanic $mechanic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Mechanic  $mechanic
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mechanic $mechanic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Mechanic  $mechanic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Mechanic $mechanic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Mechanic  $mechanic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mechanic $mechanic)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TruckController;
+use App\Http\Controllers\MechanicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'mechanics'], function(){
+    Route::get('', [MechanicController::class, 'index'])->name('mechanic.index');
+    Route::get('create', [MechanicController::class, 'create'])->name('mechanic.create');
+    Route::post('store', [MechanicController::class, 'store'])->name('mechanic.store');
+    Route::get('edit/{mechanic}', [MechanicController::class, 'edit'])->name('mechanic.edit');
+    Route::post('update/{mechanic}', [MechanicController::class, 'update'])->name('mechanic.update');
+    Route::post('delete/{mechanic}', [MechanicController::class, 'destroy'])->name('mechanic.destroy');
+    Route::get('show/{mechanic}', [MechanicController::class, 'show'])->name('mechanic.show');
+ });
+ 
+
+
